974049fd86ae4be57759319424b135a98a6e4893
M.Emre YILMAZ <memreyilmaz@gmail.com>
2021-09-24 14:47:36 +0300

    Implemented remaining query functions on Repository files

--- app/src/main/java/com/example/graphqltrial/data/repo/GitHubRepository.kt
+++ app/src/main/java/com/example/graphqltrial/data/repo/GitHubRepository.kt
@@ -1,10 +1,23 @@
 package com.example.graphqltrial.data.repo
 
 import com.apollographql.apollo.api.Response
+import com.example.graphqltrial.GetBioQuery
+import com.example.graphqltrial.GetRepositoryQuery
+import com.example.graphqltrial.GetTopicQuery
 import com.example.graphqltrial.GithubUserQuery
+import com.example.graphqltrial.SearchQuery
+import com.example.graphqltrial.type.SearchType
 
 interface GitHubRepository {
 
     suspend fun getUser(login: String) : Response<GithubUserQuery.Data>
+
+    suspend fun getBio() : Response<GetBioQuery.Data>
+
+    suspend fun getRepository(name:String, owner:String) : Response<GetRepositoryQuery.Data>
+
+    suspend fun getTopic(name: String, first: Int) : Response<GetTopicQuery.Data>
+
+    suspend fun search(query: String, type: SearchType, first: Int) : Response<SearchQuery.Data>
 
 }
--- app/src/main/java/com/example/graphqltrial/data/repo/GitHubRepositoryImpl.kt
+++ app/src/main/java/com/example/graphqltrial/data/repo/GitHubRepositoryImpl.kt
@@ -1,14 +1,49 @@
 package com.example.graphqltrial.data.repo
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Input
 import com.apollographql.apollo.api.Response
 import com.apollographql.apollo.coroutines.await
+import com.example.graphqltrial.GetBioQuery
+import com.example.graphqltrial.GetRepositoryQuery
+import com.example.graphqltrial.GetTopicQuery
 import com.example.graphqltrial.GithubUserQuery
+import com.example.graphqltrial.SearchQuery
+import com.example.graphqltrial.type.SearchType
 import javax.inject.Inject
 
 class GitHubRepositoryImpl @Inject constructor(private val client: ApolloClient) :
     GitHubRepository {
     override suspend fun getUser(login: String): Response<GithubUserQuery.Data> {
         return client.query(GithubUserQuery(login = login)).await()
+    }
+
+    override suspend fun getBio(): Response<GetBioQuery.Data> {
+        return client.query(GetBioQuery()).await()
+    }
+
+    override suspend fun getRepository(
+        name: String,
+        owner: String
+    ): Response<GetRepositoryQuery.Data> {
+        return client.query(GetRepositoryQuery(name = name, owner = owner)).await()
+    }
+
+    override suspend fun getTopic(name: String, first: Int): Response<GetTopicQuery.Data> {
+        return client.query(GetTopicQuery(name = name, first = Input.fromNullable(first))).await()
+    }
+
+    override suspend fun search(
+        query: String,
+        type: SearchType,
+        first: Int
+    ): Response<SearchQuery.Data> {
+        return client.query(
+            SearchQuery(
+                query = query,
+                type = type,
+                first = Input.fromNullable(first)
+            )
+        ).await()
     }
 }
